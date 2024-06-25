75c71881b58b22822664eeb4553d6b8cbb7734cb
Felix Geiss <geissq@gmail.co>
2024-06-25 14:27:16 +0200

    Logo + Burgermenu + PizzaPic + Resturantname

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 
 
+
+function init () {
+    let topImg = document.getElementById('topContainer')
+    let info = document.getElementById('infoContainer')
+    topImg.innerHTML = ` <img src="img/pizza-5275191_1280.jpg" alt="">`
+    info.innerHTML = '<h2>Osteria Bella Napoli</h2>'
+}
+
 // BurgerMenu
-function toggleIcon() {
-    document.getElementById("navIcon").classList.toggle("open");
-  }
+document.getElementById("navIcon").classList.toggle("open");
 
 
 function toggleMenu() {
