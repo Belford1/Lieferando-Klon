eefda0aa4bb1f9759c36ef13d3233f12854ba334
Felix Geiss <geissq@gmail.co>
2024-06-25 16:02:10 +0200

    Stars + Heart+ I

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 
 
 
-function init () {
-    let topImg = document.getElementById('topContainer')
-    let info = document.getElementById('infoContainer')
-    topImg.innerHTML = ` <img src="img/pizza-5275191_1280.jpg" alt="">`
-    info.innerHTML = '<h2>Osteria Bella Napoli</h2>'
+function init() {
+    let topImg = document.getElementById('topContainer');
+    let info = document.getElementById('infoContainer');
+    
+    topImg.innerHTML = '<img src="img/pizza-5275191_1280.jpg" alt="">';
+    info.innerHTML = `
+<div class="info">
+<div>   
+<h2>Osteria Bella Napoli</h2>
+    <img class="stars" src="img/bewertung.png" alt="">
+</div>
+<div class="infoIqon">
+    <div>
+        <span>I</span>
+    </div>
+    <div>
+        <img src="img/mogen.png" alt="">
+    </div>
+</div>
+
+</div>
+    `;
 }
+
 
 // BurgerMenu
 document.getElementById("navIcon").classList.toggle("open");
