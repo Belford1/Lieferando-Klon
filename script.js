e9af94163e28b4289da2729e1844b2152b1639d8
Felix Geiss <geissq@gmail.co>
2024-07-07 14:35:29 +0200

    Mobile menu repaired and adjusted

--- script.js
+++ script.js
@@ -159,13 +159,15 @@ function toggleBasket() {
 }
 
 // BurgerMenu
-// document.getElementById("navIcon").classList.toggle("open");
-
-// function toggleMenu() {
-//   let menu = document.getElementById("menu");
-//   if (menu.style.width === "250px") {
-//     menu.style.width = "0";
-//   } else {
-//     menu.style.width = "250px";
-//   }
-// }
+function toggleIcon() {
+    document.getElementById("navIcon").classList.toggle("open");
+  }
+  
+  function toggleMenu() {
+    var menu = document.getElementById("menu");
+    if (menu.style.width === "100%") {
+      menu.style.width = "0";
+    } else {
+      menu.style.width = "100%";
+    }
+  }
